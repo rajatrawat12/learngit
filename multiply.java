b363dea06a777c1c2d31a7ed91ce8569d0b48edb
rajat <rawatrajat300@gmail.com>
2022-08-11 22:55:55 +0530

    multiply

--- multiply.java
+++ multiply.java
@@ -1,7 +1,7 @@
-public class sum {
+public class multiply {
     public static void main(string[]args){
     int a=9;
     int b=8;
-    system.out.println("the sum is"+(a+b));
+    system.out.println("the product is"+(a*b));
 }
 }
